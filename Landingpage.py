034d5e819b9ab5a4d9fdf0075853b21da932267d
xosolutions <84718322+xosolutions@users.noreply.github.com>
2026-06-25 23:12:07 +0530

    Update Landingpage.py

--- Landingpage.py
+++ Landingpage.py
@@ -9,7 +9,7 @@ st.markdown(
   """,
   unsafe_allow_html=True,
 )
-robo = genai.Client(api_key="MY_APKEY")
+robo = genai.Client(api_key=st.secrets["MY_APKEY"])
 mychat = robo.chats.create(model="gemini-flash-lite-latest")
 #Placeholder for the response
 response_placeholder = st.empty()
